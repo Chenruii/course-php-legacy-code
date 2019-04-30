3485bb8aa2f5208462e851712b6bb6702d0dcb1d
Chenruii <rui.chen1996@gmail.com>
2019-04-30 12:07:39 +0200

    ajout de container service

---
+++ www/config/di.global.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 30/04/2019
+ * Time: 10:06
+ */
+
+use Controller\PagesController;
+use Controller\UsersController as UsersControllerAlias;
+use Models\Users;
+
+// c'est un require: il retourner le tableau directmeent
+return[
+    Users::class => function($container){
+        $host = $container['config']['database']['host'];
+        $driver = $container['config']['database']['driver'];
+        $name = $container['config']['database']['name'];
+        $user = $container['config']['database']['user'];
+        $password = $container['config']['database']['password'];
+
+        return new Users($driver,$host,$name,$user,$password);
+
+    },
+
+    UsersControllerAlias::class => function($container){
+    $userModel = $container[Users::class]($container);
+
+    return new UsersControllerAlias($userModel);
+    },
+    PagesController::class => function($container){
+        return new PagesController();
+    }
+
+];
+
---
+++ www/config/global.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 30/04/2019
+ * Time: 10:24
+ */
+return[
+    'database' => [
+        'driver' => 'mysql',
+        'host' => 'database',
+        'name' => 'course-php-legacy',
+        'user' => 'root',
+        'password' => 'course-legacy',
+    ],
+];
