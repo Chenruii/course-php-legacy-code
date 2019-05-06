113cabff90c92efdd4b314eb03c2c8ae29b45f4a
Chenruii <rui.chen1996@gmail.com>
2019-05-07 01:37:43 +0200

    user variables

---
+++ www/Controller/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controller;
+
+use App\Core\Validator;
+use App\Core\View;
+use App\Models\Users;
+
+class UsersController extends Controller
+{
+    public function defaultAction(): void
+    {
+        echo 'users default';
+    }
+
+
+
+
+}
---
+++ www/Entity/User.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 07/05/2019
+ * Time: 01:18
+ */
+namespace App\Enitty;
+
+
+class User implements UserInterface
+{
+    private id;
+    private firstname;
+    private lastnamemm
+    private email;
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
