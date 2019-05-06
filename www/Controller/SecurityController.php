d0221cf328c6bb08da05d5349a921e86e9333a0f
Chenruii <rui.chen1996@gmail.com>
2019-05-07 01:27:33 +0200

    securityController : login add view action

---
+++ www/Controller/SecurityController.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 07/05/2019
+ * Time: 01:24
+ */
+
+namespace Controller;
+
+
+class SecurityController
+{
+    public function addAction(): void
+    {
+        $user = new Users();
+        $form = $user->getRegisterForm();
+
+        $v = new View('addUser', 'front');
+        $v->assign('form', $form);
+    }
+
+    public function saveAction(): void
+    {
+        $user = new Users();
+        $form = $user->getRegisterForm();
+        $method = strtoupper($form['config']['method']);
+        $data = $GLOBALS['_'.$method];
+
+        if ($_SERVER['REQUEST_METHOD'] == $method && !empty($data)) {
+            $validator = new Validator($form, $data);
+            $form['errors'] = $validator->errors;
+
+            if (empty($errors)) {
+                $user->setFirstname($data['firstname']);
+                $user->setLastname($data['lastname']);
+                $user->setEmail($data['email']);
+                $user->setPwd($data['pwd']);
+                $user->save();
+            }
+        }
+
+        $v = new View('addUser', 'front');
+        $v->assign('form', $form);
+    }
+
+    public function loginAction(): void
+    {
+        $user = new Users();
+        $form = $user->getLoginForm();
+
+        $method = strtoupper($form['config']['method']);
+        $data = $GLOBALS['_'.$method];
+        if ($_SERVER['REQUEST_METHOD'] == $method && !empty($data)) {
+            $validator = new Validator($form, $data);
+            $form['errors'] = $validator->errors;
+
+            if (empty($errors)) {
+                $token = md5(substr(uniqid().time(), 4, 10).'mxu(4il');
+                // TODO: connexion
+            }
+        }
+
+        $v = new View('loginUser', 'front');
+        $v->assign('form', $form);
+    }
+
+    public function forgetPasswordAction(): void
+    {
+        $v = new View('forgetPasswordUser', 'front');
+    }
+
+}
